d21d93a114e38eef2c408073b71d3aabeefd0d52
DmitriyB763 <mr.diy3292@gmail.com>
2024-01-09 20:47:20 +0400

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+int priceTicket = 20;
+int bonusRub = 20;
+
+int bonusMiles = priceTicket/bonusRub;
+        System.out.println(bonusMiles);
+    }
+}
